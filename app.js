799ad20790d8caa77505e625841a176a57192b65
MuzanCoder <136102700+MuzanCoder@users.noreply.github.com>
2023-07-15 15:50:50 +0530

    Input Output Processing.

--- app.js
+++ app.js
@@ -1,12 +1,10 @@
 var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
-
-console.log(txtInput)
+var outputDiv = document.querySelector("#output");
 
 
 function clickHandler() {
-    console.log("clicked!");
-    console.log("Input", txtInput.value);
+    outputDiv.innerText = "xyz " + txtInput.value;
 };
 
 
